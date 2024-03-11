092eaaf31b78d2996981caf8a05221d88a0c4b7d
Ville Jänkälä <ville.jankala@gmail.com>
2024-03-11 17:32:40 +0200

    dev experience improvements

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
+import checker from "vite-plugin-checker";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    checker({
+      typescript: true,
+    }),
     react(),
     VitePWA({
       manifest: {
